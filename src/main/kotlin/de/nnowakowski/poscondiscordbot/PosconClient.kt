8779c5a3c5c3d59ec6d9b2e860c88406caba6ae2
Nico Nowakowski <nnico.nnowakowski@gmail.com>
2021-12-17 09:05:49 +0100

    Added retry functionality in case the API call fails

--- src/main/kotlin/de/nnowakowski/poscondiscordbot/PosconClient.kt
+++ src/main/kotlin/de/nnowakowski/poscondiscordbot/PosconClient.kt
@@ -6,12 +6,17 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToFlux
 import reactor.core.publisher.Flux
+import reactor.util.retry.Retry
+import java.time.Duration
 
 @Component
 class PosconClient(@Autowired private val webClientBuilder: WebClient.Builder) {
-    private val webClient: WebClient = webClientBuilder.baseUrl("https://hqapi.poscon.net/").build();
+    private val webClient: WebClient = webClientBuilder.baseUrl("https://hqapi.poscon.net/").build()
 
     fun getOnlineUsers(): Flux<PosconOnlineResponse> {
-        return webClient.get().uri("online.json").retrieve().bodyToFlux()
+        return webClient.get().uri("online.json").retrieve().bodyToFlux<PosconOnlineResponse>()
+            .retryWhen(Retry.backoff(5, Duration.ofSeconds(2)).doBeforeRetry {
+                println("Couldn't reach API, retrying ...")
+            })
     }
 }
